e39edba35ac0ee3b9424c4440a325593b94a375a
Nikolay Sosin <nmsosin@yandex.ru>
2024-07-26 16:07:05 +0300

    feat: add code export feature

--- src/components/preview/preview-form.tsx
+++ src/components/preview/preview-form.tsx
@@ -4,10 +4,45 @@ import { useAppSelector } from "../../utils/hooks";
 function PreviewForm() {
   const inputList = useAppSelector(state => state.inputs.inputList);
 
+  const isFormFilled = inputList.length > 0;
+
+  const handleSaveLayout = () => {
+    const code = document.getElementById('codeOutput')!.outerHTML;
+    const blob = new Blob([code!], { type: 'text/plain' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'layout.html';
+    link.click();
+    URL.revokeObjectURL(link.href);
+  };
+
+  const handleSaveStyles = () => {
+    const elt = document.getElementById('codeOutput');
+    const styles = window.getComputedStyle(elt!);
+    const code = Object.entries(styles!).map(([k, v]) => {
+      k = k.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`);
+      return `${k}:${v}`
+  }).join(';');
+    const blob = new Blob([code!], { type: 'text/plain' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'styles.css';
+    link.click();
+    URL.revokeObjectURL(link.href);
+  };
+
   return (
-    <>
-      <form action='submit'>
-        <h2 className={styles.sectionTitle}>Предварительный просмотр формы</h2>
+    <div>
+      <h2 className={styles.sectionTitle}>Предварительный просмотр формы</h2>
+      <div className={styles.exportButtons}>
+          <button onClick={handleSaveLayout} disabled={!isFormFilled}>
+            Сохранить html
+          </button>
+          <button onClick={handleSaveStyles} disabled={!isFormFilled}>
+            Сохранить css
+          </button>
+        </div>
+      <form action='submit' id="codeOutput">
         <div className={styles.container}>
           {inputList.length > 0 && inputList.map((item, idx) => {
             if (item.type === 'text') {
@@ -31,12 +66,10 @@ function PreviewForm() {
               </select>
             }
             
-
-
           })}
         </div>
       </form>
-    </>
+    </div>
   )
 }
 
